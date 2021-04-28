97922c0b05874b6d2b1fd1fe222d6554fef73190
nanajr-dotcom <asamoah@goldmail.etsu.edu>
2021-04-28 13:12:13 +0000

    initia commit

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCjWltKAsccX-KkJs3xHyFCRsB2TOfGcqI",
+  authDomain: "clone-ffb16.firebaseapp.com",
+  projectId: "clone-ffb16",
+  storageBucket: "clone-ffb16.appspot.com",
+  messagingSenderId: "8607103665",
+  appId: "1:8607103665:web:5a497ae7e687919f6a4090",
+  measurementId: "G-1EPFGHPTRX"
+};
