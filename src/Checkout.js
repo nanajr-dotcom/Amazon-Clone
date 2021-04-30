0a6695a5081a8d1371b33dc6e69776b6b0f7c70d
nanajr-dotcom <asamoah@goldmail.etsu.edu>
2021-04-30 08:23:19 +0000

    router added

---
+++ src/Checkout.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Checkout.css';
+
+function Checkout() {
+    return (
+        <div>
+            <h1>
+                Checkout in a bit
+            </h1>
+        </div>
+    )
+}
+
+export default Checkout
--- src/Home.js
+++ src/Home.js
@@ -12,13 +12,15 @@ function Home() {
               alt="" />   
               <div className="home__row">
                   <Product 
-                  title="Rich Dad Poor Dad"
+                  id="24555"
+                  title="Rich Dad Poor Dad: What the Rich Teach Their Kids About Money That the Poor and Middle Class Do Not!"
                   price="29.99"
                   rating={4}
                   image="https://m.media-amazon.com/images/I/51u8ZRDCVoL._AC_SY200_.jpg"
                   />
                   <Product
-                  title="PS 5 Controller"
+                  id="24557"
+                  title="Playstation DualSense Wireless Controller"
                   price="59.99"
                   rating={5}
                   image="https://m.media-amazon.com/images/I/41McnBJl0FL._AC_SY200_.jpg"
@@ -27,31 +29,35 @@ function Home() {
               </div>
               <div className="home__row" >
                   <Product
-                   title="Nike Boost"
-                  price="49.99"
+                  id="24558"
+                  title="2020 Apple iPad Pro (11-inch, Wi-Fi, 128GB) - Space Gray (2nd Generation)"
+                  price="749.99"
                   rating={5}
-                  image="https://m.media-amazon.com/images/I/41-1qBK2E3L._AC_SY200_.jpg"
+                  image="https://m.media-amazon.com/images/I/815ztYEEwYL._AC_UL320_.jpg"
                   />
                   <Product
-                   title="Roku TV Ultra"
+                  id="24559"
+                   title="Roku Ultra 2020 | Streaming Media Player HD/4K/HDR/Dolby Vision with Dolby Atmos, Bluetooth Streaming"
                   price="74.99"
-                  rating={5}
+                  rating={3}
                   image="https://m.media-amazon.com/images/I/41LOLsICjpL._AC_SY200_.jpg"
                   />
                   <Product
-                   title="Anne Klien Watch Set"
+                  id="24560"
+                   title=" Anne Klein Women's Swarovski Crystal Accented Watch and Bangle Set, AK/3288"
                   price="159.99"
-                  rating={5}
+                  rating={3}
                   image="https://m.media-amazon.com/images/I/41PQOlZ00eL._AC_SY200_.jpg"
                   />
                   
               </div>
               <div className="home__row">
                   <Product
-                   title="LG HD Monitor 32inches"
+                  id="24561"
+                   title="LG 27GL83A-B 27 Inch Ultragear QHD IPS 1ms NVIDIA G-SYNC Compatible Gaming Monitor, Black"
                   price="129.99"
                   rating={5}
-                  image="https://m.media-amazon.com/images/I/31PTviHMeUL._AC_SY200_.jpg"
+                  image="https://m.media-amazon.com/images/I/91lS5yEDH-L._AC_UY218_.jpg"
                   />
                   
               </div>
